ffe1f480f8f58b8610a966ceffe3fec3abbd578b
BhuvanaGN <gnbhuvanaa@gmail.com>
2019-05-25 13:36:31 +0530

    Till Button with FlowerImage

--- app/src/main/java/in/bhuvana/flipacoinjava/FlipCoinActivity.java
+++ app/src/main/java/in/bhuvana/flipacoinjava/FlipCoinActivity.java
@@ -2,12 +2,39 @@ package in.bhuvana.flipacoinjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class FlipCoinActivity extends AppCompatActivity {
+
+    private static final Random RANDOM= new Random();
+    private ImageView coinSide;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flip_coin);
+        setContentView(R.layout.activity_flip_linear);
+        coinSide=(ImageView) findViewById(R.id.imageViewHead);
+        btn=(Button) findViewById(R.id.FlipcoinButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipCoin();
+            }
+        });
     }
-}
+
+        private void flipCoin() {
+        coinSide.setImageResource(RANDOM.nextFloat() > 0.5f ? R.drawable.flowerofcoin : R.drawable.headofcoin);
+    }
+
+    }
+
+
+
+
+
